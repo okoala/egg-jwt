8bacdb22947c655b29e6a75b27b8f3d123d68157
zhong666 <aa900031@gmail.com>
2017-06-19 17:19:19 +0800

    兼容舊的寫法

--- app/extend/application.js
+++ app/extend/application.js
@@ -1,12 +1,16 @@
 'use strict';
 
-const JWT = Symbol('Application#jwt');
+const koajwt = require('koa-jwt');
 const jwt = require('jsonwebtoken');
+const JWT = Symbol('Application#jwt');
 
 module.exports = {
   get jwt() {
     if (!this[JWT]) {
-      this[JWT] = jwt;
+      this[JWT] = koajwt(this.config.jwt);
+      this[JWT].sign = jwt.sign;
+      this[JWT].verify = jwt.verify;
+      this[JWT].decode = jwt.decode;
     }
     return this[JWT];
   },
