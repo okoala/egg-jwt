8707195d19ada57aa1abb397931b8bced8fcadfa
okoala <dapixp@gmail.com>
2018-01-16 18:44:18 +0800

    feat: optional push jwt after cors in coreMiddleware

--- app.js
+++ app.js
@@ -7,6 +7,17 @@ const MIDDLEWARE_NAME_JWT = 'jwt';
 module.exports = app => {
   const index = app.config.appMiddleware.indexOf(MIDDLEWARE_NAME_JWT);
 
-  assert.equal(index, -1, `Duplication of middleware name found: ${MIDDLEWARE_NAME_JWT}. Rename your middleware other than "${MIDDLEWARE_NAME_JWT}" please.`);
-  app.config.coreMiddleware.unshift(MIDDLEWARE_NAME_JWT);
+  assert.equal(
+    index,
+    -1,
+    `Duplication of middleware name found: ${MIDDLEWARE_NAME_JWT}. Rename your middleware other than "${MIDDLEWARE_NAME_JWT}" please.`
+  );
+
+  const corsIndex = app.config.coreMiddleware.indexOf('cors');
+  if (corsIndex > -1) {
+    // push jwt after cors
+    app.config.coreMiddleware.splice(corsIndex + 1, 0, MIDDLEWARE_NAME_JWT);
+  } else {
+    app.config.coreMiddleware.unshift(MIDDLEWARE_NAME_JWT);
+  }
 };
