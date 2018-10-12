1fd6e0e7e122125984f5b48ce38ee9ff79cc4ece
Leung-JZ <375082677@qq.com>
2018-10-12 16:57:32 +0800

    Update index.d.ts
    update chinese to english

--- index.d.ts
+++ index.d.ts
@@ -3,9 +3,9 @@ declare module "egg" {
     jwt: {
       /**
        *
-       * @param payload 待签名的数据
-       * @param secretOrPrivateKey 签名秘钥。string or { key, passphrase }
-       * @param options jwt 选项。see more details in https://github.com/auth0/node-jsonwebtoken
+       * @param payload datas. datas to be signed
+       * @param secretOrPrivateKey secret key. string or { key, passphrase }
+       * @param options jwt options。see more details in https://github.com/auth0/node-jsonwebtoken
        * @param callback callback
        */
       sign(
@@ -16,9 +16,9 @@ declare module "egg" {
       ): string;
       /**
        *
-       * @param token jwt token.
-       * @param secretOrPrivateKey 签名秘钥。string or { key, passphrase }
-       * @param options jwt 选项。see more details in https://github.com/auth0/node-jsonwebtoken
+       * @param token jwt token. 
+       * @param secretOrPrivateKey secret key。string or { key, passphrase }
+       * @param options jwt options。see more details in https://github.com/auth0/node-jsonwebtoken
        * @param callback callback
        */
       verify(
