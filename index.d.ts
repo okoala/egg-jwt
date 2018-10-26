b4b25879abab0330ac8531b1540e7aabd2b1531c
gaoletian <virtoolswebplayer@gmail.com>
2018-10-26 16:19:42 +0800

    feat(type):
    1. secretOrPrivateKey 选项可选 ， 2. 添加 decode 函数声明

--- index.d.ts
+++ index.d.ts
@@ -2,7 +2,7 @@ declare module "egg" {
   interface Application {
     jwt: {
       /**
-       *
+       * 签名
        * @param payload datas. datas to be signed
        * @param secretOrPrivateKey secret key. string or { key, passphrase }
        * @param options jwt options。see more details in https://github.com/auth0/node-jsonwebtoken
@@ -10,12 +10,12 @@ declare module "egg" {
        */
       sign(
         payload: any,
-        secretOrPrivateKey: string,
+        secretOrPrivateKey?: string,
         options?: any,
         callback?: function
       ): string;
       /**
-       *
+       * 较验
        * @param token jwt token. 
        * @param secretOrPrivateKey secret key。string or { key, passphrase }
        * @param options jwt options。see more details in https://github.com/auth0/node-jsonwebtoken
@@ -23,10 +23,16 @@ declare module "egg" {
        */
       verify(
         token: any,
-        secretOrPrivateKey: string,
+        secretOrPrivateKey?: string,
         options?: any,
         callback?: function
       ): string;
+      
+      /**
+	     * 解码
+	     * @param token jwt token
+	     */
+	    decode(token: string): string;
     };
   }
 }
