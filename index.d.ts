198f46b164deb595f158c955df0ecc1790b69d20
gaoletian <virtoolswebplayer@gmail.com>
2019-02-21 11:34:20 +0800

    feat: update index.d.ts  (#34)
    * fix(package): npm发包丢失index.d.ts
    
    修复npm 发包缺少 index.d.ts
    
    * feat(type): 
    
    1. secretOrPrivateKey 选项可选 ， 2. 添加 decode 函数声明
    
    * feat: 格式化类型定义文件
    
    * fix: 使用英文注释
    
    使用英文注释

--- index.d.ts
+++ index.d.ts
@@ -3,7 +3,7 @@ declare module 'egg' {
   interface Application {
     jwt: {
       /**
-       *
+       * call jsonwebtoken's sign() method
        * @param payload datas. datas to be signed
        * @param secretOrPrivateKey secret key. string or { key, passphrase }
        * @param options jwt options。see more details in https://github.com/auth0/node-jsonwebtoken
@@ -16,13 +16,19 @@ declare module 'egg' {
         callback?: SignCallback
       ): string;
       /**
-       *
+       * call jsonwebtoken's verify() method
        * @param token jwt token. 
        * @param secretOrPrivateKey secret key。string or { key, passphrase }
        * @param options jwt options。see more details in https://github.com/auth0/node-jsonwebtoken
        * @param callback callback
        */
       verify(token: string, secretOrPrivateKey: string, options?: VerifyOptions, callback?: VerifyCallback): string;
+
+      /**
+       * call jsonwebtoken's decode() method
+       * @param token jwt token
+       */
+      decode(token: string): string;
     };
   }
   interface EggAppConfig {
